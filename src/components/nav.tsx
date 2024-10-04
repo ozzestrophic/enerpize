fd92be523b856fc11dd84c2628dca6a3e2c04544
Ozzeek <osama.m.alnagar@gmail.com>
2024-10-04 23:06:40 +0300

    prettier tailwind

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -7,30 +7,32 @@ const link = ["Modules", "Industries", "Pricing"];
 
 const Nav = () => {
   return (
-    <nav className="max-w-7xl px-16 py-4 border-b border-gray-200">
-      <div className="flex items-center justify-between w-full">
-        <div className="flex items-center gap-8">
-          <Image
-            className="w-20 md:w-32"
-            src={"/enerpize-logo.png"}
-            alt="Enerpize Logo"
-            width={120}
-            height={120}
-          />
-          <div className="flex gap-6">
-            {link.map((item) => (
-              <Link className="font-medium" href="/" key={item}>
-                {item}
-              </Link>
-            ))}
+    <nav className="border-b border-gray-200 px-16 py-4">
+      <div className="mx-auto max-w-7xl">
+        <div className="flex w-full items-center justify-between">
+          <div className="flex items-center gap-8">
+            <Image
+              className="w-20 md:w-32"
+              src={"/enerpize-logo.png"}
+              alt="Enerpize Logo"
+              width={120}
+              height={120}
+            />
+            <div className="flex gap-6">
+              {link.map((item) => (
+                <Link className="font-medium" href="/" key={item}>
+                  {item}
+                </Link>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="flex gap-4">
-          <Button variant={"ghost"}>
-            <DoorOpen className="w-4 h-4 mr-2" />
-            text
-          </Button>
-          <Button>Try for free</Button>
+          <div className="flex gap-4">
+            <Button variant={"ghost"}>
+              <DoorOpen className="mr-2 h-4 w-4" />
+              text
+            </Button>
+            <Button>Try for free</Button>
+          </div>
         </div>
       </div>
     </nav>
