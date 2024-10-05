f4021bf7b24eaff00afb8f44f82cb917c35aa1d5
Ozzeek <osama.m.alnagar@gmail.com>
2024-10-05 03:24:10 +0300

    button to sales section

--- src/components/sales-section.tsx
+++ src/components/sales-section.tsx
@@ -2,11 +2,13 @@ import MaxWidthWrapper from "@/components/max-width-wrapper";
 import SectionHeading from "@/components/section-heading";
 import SalesItems from "./sales-items";
 import Image from "next/image";
+import { Button } from "./ui/button";
+import { ArrowRight } from "lucide-react";
 
 const SalesSection = () => {
   return (
     <MaxWidthWrapper>
-      <section className="grid grid-cols-1 gap-12 py-20 md:grid-cols-2 md:gap-8">
+      <section className="grid grid-cols-1 items-center gap-12 py-20 md:grid-cols-2 md:gap-8">
         <div className="row-start-2 space-y-10 md:row-start-1">
           <SectionHeading
             tag="Sales"
@@ -14,6 +16,13 @@ const SalesSection = () => {
             description=""
           />
           <SalesItems />
+          <Button
+            variant={"secondary"}
+            className="h-auto w-full justify-between px-6 py-5 text-xl font-bold"
+          >
+            Start Selling for Free
+            <ArrowRight className="h-6 w-6" />
+          </Button>
         </div>
         <div className="relative mx-8 md:mx-0">
           <Image
