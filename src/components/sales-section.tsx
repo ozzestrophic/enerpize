5cf61c9f581d1aa2e6d645c1302c86e711c505c7
Ozzeek <osama.m.alnagar@gmail.com>
2024-10-05 22:15:40 +0300

    animate sales section

--- src/components/sales-section.tsx
+++ src/components/sales-section.tsx
@@ -1,14 +1,40 @@
+"use client";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import SectionHeading from "@/components/section-heading";
 import SalesItems from "./sales-items";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { ArrowRight } from "lucide-react";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
+
+const initial = {
+  opacity: 0,
+  y: 100,
+};
+
+const animate = {
+  opacity: 1,
+  y: 0,
+};
+
+const transition = {
+  duration: 0.5,
+  ease: "easeOut",
+};
 
 const SalesSection = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, margin: "-200px" });
   return (
     <MaxWidthWrapper>
-      <section className="grid grid-cols-1 items-center gap-12 py-20 md:grid-cols-2 md:gap-8 md:py-40">
+      <motion.section
+        ref={ref}
+        initial={initial}
+        animate={isInView && animate}
+        transition={transition}
+        className="grid grid-cols-1 items-center gap-12 py-20 md:grid-cols-2 md:gap-8 md:py-40"
+      >
         <div className="row-start-2 space-y-10 md:row-start-1">
           <SectionHeading
             tag="Sales"
@@ -40,7 +66,7 @@ const SalesSection = () => {
             height={1000}
           />
         </div>
-      </section>
+      </motion.section>
     </MaxWidthWrapper>
   );
 };
