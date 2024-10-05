ac44e89eb802154e2befd7db12c3042e704da099
Ozzeek <osama.m.alnagar@gmail.com>
2024-10-05 22:54:28 +0300

    shadow to accounting images

--- src/components/accounting-images.tsx
+++ src/components/accounting-images.tsx
@@ -7,19 +7,19 @@ const AccountingImages = () => {
       <div className="relative space-y-8 md:w-full">
         <motion.div
           whileHover={{ scale: 1.05, rotate: -4 }}
-          className="relative left-0 top-0 w-4/5"
+          className="relative left-0 top-0 w-4/5 shadow-lg shadow-slate-300"
         >
           <Image src={"/chart1.png"} alt="sales" width={1000} height={1000} />
         </motion.div>
         <motion.div
           whileHover={{ scale: 1.05, rotate: 4 }}
-          className="top-1/5 relative left-[20%] w-4/5"
+          className="top-1/5 relative left-[20%] w-4/5 shadow-lg shadow-slate-300"
         >
           <Image src={"/chart2.png"} alt="sales" width={1000} height={1000} />
         </motion.div>
         <motion.div
           whileHover={{ scale: 1.05, rotate: -4 }}
-          className="relative bottom-0 left-0 w-4/5"
+          className="relative bottom-0 left-0 w-4/5 shadow-lg shadow-slate-300"
         >
           <Image src={"/chart3.png"} alt="sales" width={1000} height={1000} />
         </motion.div>
